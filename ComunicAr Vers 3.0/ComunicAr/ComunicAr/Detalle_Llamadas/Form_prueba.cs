868919877cd16ae7c4fb0388ab8b9591c371112c
maticeba <71660@sistemas.frc.utn.edu.ar>
2021-05-19 14:54:28 -0300

    Prueba_Rama

---
+++ ComunicAr Vers 3.0/ComunicAr/ComunicAr/Detalle_Llamadas/Form_prueba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComunicAr.Detalle_Llamadas
+{
+    public partial class Form_prueba : Form
+    {
+        public Form_prueba()
+        {
+            InitializeComponent();
+        }
+    }
+}
