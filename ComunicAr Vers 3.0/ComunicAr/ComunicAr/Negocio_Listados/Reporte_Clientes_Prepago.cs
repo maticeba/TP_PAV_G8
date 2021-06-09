67e2683e390582a93f0239ae44725a6991a530c1
Tomassalomon <turcosalomon1@gmail.com>
2021-06-09 20:13:01 -0300

    Prepago "todos" arreglado

--- ComunicAr Vers 3.0/ComunicAr/ComunicAr/Negocio_Listados/Reporte_Clientes_Prepago.cs
+++ ComunicAr Vers 3.0/ComunicAr/ComunicAr/Negocio_Listados/Reporte_Clientes_Prepago.cs
@@ -14,10 +14,12 @@ namespace ComunicAr.Negocio_Listados
 
         public DataTable ReporteDatos_todos()
         {
-            string sql = "SELECT c.nombre_razonSocial, n.nro_telefono, sp.descripcion, sp.costo " +
+            string sql = "SELECT c.nombre_razonSocial, sp.descripcion, sp.costo, " +
+                      "('+' + n.cod_nacional + ' ' +  n.cod_area + ' ' + n.nro_telefono) AS nro_telefono " +
                       "FROM Cliente c, Numero n, Servicios_contratados sc, Servicios_prepago sp " +
                       "WHERE c.nro_cliente = n.nro_cliente" +
-                      " AND n.id_numero = sc.id_numero AND sc.tipo_servicio = sp.tipo_servicio " +
+                      " AND n.id_numero = sc.id_numero AND sc.tipo_servicio = sp.tipo_servicio" +
+                      " AND sc.id_servicio = sp.id_pack " +
                       "ORDER BY c.nro_cliente";
             DataTable tabla = new DataTable();
             tabla = BD.EjecutarSelect(sql);
