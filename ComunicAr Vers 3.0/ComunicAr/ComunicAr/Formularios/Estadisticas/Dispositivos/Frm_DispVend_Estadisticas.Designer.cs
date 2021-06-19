addb4ef130b2d71a0a4dc674bf9b38c976661b3d
AleFontao <82983855+AleFontao@users.noreply.github.com>
2021-06-19 10:40:49 -0300

    20.3

---
+++ ComunicAr Vers 3.0/ComunicAr/ComunicAr/Formularios/Estadisticas/Dispositivos/Frm_DispVend_Estadisticas.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ComunicAr.Formularios.Estadisticas.Dispositivos
+{
+    partial class Frm_DispVend_Estadisticas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 84);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(26, 131);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(603, 382);
+            this.reportViewer1.TabIndex = 1;
+            // 
+            // Frm_DispVend_Estadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 593);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Frm_DispVend_Estadisticas";
+            this.Text = "Frm_DispVend_Estadisticas";
+            this.Load += new System.EventHandler(this.Frm_DispVend_Estadisticas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ ComunicAr Vers 3.0/ComunicAr/ComunicAr/Formularios/Estadisticas/Dispositivos/Frm_DispVend_Estadisticas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComunicAr.Formularios.Estadisticas.Dispositivos
+{
+    public partial class Frm_DispVend_Estadisticas : Form
+    {
+        public Frm_DispVend_Estadisticas()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_DispVend_Estadisticas_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
