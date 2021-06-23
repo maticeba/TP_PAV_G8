3a1f378f05d3a441bbdc95301bbf85a2f746cc43
FranMarcuzzi <franmarcuzzi@icloud.com>
2021-06-23 19:52:57 -0300

    22.3.1

--- ComunicAr Vers 3.0/ComunicAr/ComunicAr/Formularios/Menu/Frm_Menu_Estadisticas.Designer.cs
+++ ComunicAr Vers 3.0/ComunicAr/ComunicAr/Formularios/Menu/Frm_Menu_Estadisticas.Designer.cs
@@ -35,7 +35,6 @@ namespace ComunicAr.Formularios.Menu
             this.bnt_menu_esta_llamadas = new System.Windows.Forms.Button();
             this.bnt_menu_esta_servicios = new System.Windows.Forms.Button();
             this.bnt_menu_esta_numero = new System.Windows.Forms.Button();
-            this.bnt_menu_esta_factura = new System.Windows.Forms.Button();
             this.bnt_menu_esta_cliente = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -55,7 +54,7 @@ namespace ComunicAr.Formularios.Menu
             this.bnt_menu_esta_dispositivos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.bnt_menu_esta_dispositivos.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bnt_menu_esta_dispositivos.Image = ((System.Drawing.Image)(resources.GetObject("bnt_menu_esta_dispositivos.Image")));
-            this.bnt_menu_esta_dispositivos.Location = new System.Drawing.Point(151, 126);
+            this.bnt_menu_esta_dispositivos.Location = new System.Drawing.Point(75, 126);
             this.bnt_menu_esta_dispositivos.Name = "bnt_menu_esta_dispositivos";
             this.bnt_menu_esta_dispositivos.Size = new System.Drawing.Size(115, 108);
             this.bnt_menu_esta_dispositivos.TabIndex = 1;
@@ -71,7 +70,7 @@ namespace ComunicAr.Formularios.Menu
             this.bnt_menu_esta_llamadas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.bnt_menu_esta_llamadas.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bnt_menu_esta_llamadas.Image = ((System.Drawing.Image)(resources.GetObject("bnt_menu_esta_llamadas.Image")));
-            this.bnt_menu_esta_llamadas.Location = new System.Drawing.Point(272, 126);
+            this.bnt_menu_esta_llamadas.Location = new System.Drawing.Point(196, 126);
             this.bnt_menu_esta_llamadas.Name = "bnt_menu_esta_llamadas";
             this.bnt_menu_esta_llamadas.Size = new System.Drawing.Size(115, 108);
             this.bnt_menu_esta_llamadas.TabIndex = 1;
@@ -87,7 +86,7 @@ namespace ComunicAr.Formularios.Menu
             this.bnt_menu_esta_servicios.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.bnt_menu_esta_servicios.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bnt_menu_esta_servicios.Image = ((System.Drawing.Image)(resources.GetObject("bnt_menu_esta_servicios.Image")));
-            this.bnt_menu_esta_servicios.Location = new System.Drawing.Point(30, 126);
+            this.bnt_menu_esta_servicios.Location = new System.Drawing.Point(272, 12);
             this.bnt_menu_esta_servicios.Name = "bnt_menu_esta_servicios";
             this.bnt_menu_esta_servicios.Size = new System.Drawing.Size(115, 108);
             this.bnt_menu_esta_servicios.TabIndex = 1;
@@ -113,23 +112,6 @@ namespace ComunicAr.Formularios.Menu
             this.bnt_menu_esta_numero.UseVisualStyleBackColor = false;
             this.bnt_menu_esta_numero.Click += new System.EventHandler(this.bnt_menu_esta_numero_Click);
             // 
-            // bnt_menu_esta_factura
-            // 
-            this.bnt_menu_esta_factura.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.bnt_menu_esta_factura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bnt_menu_esta_factura.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bnt_menu_esta_factura.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.bnt_menu_esta_factura.Image = ((System.Drawing.Image)(resources.GetObject("bnt_menu_esta_factura.Image")));
-            this.bnt_menu_esta_factura.Location = new System.Drawing.Point(272, 12);
-            this.bnt_menu_esta_factura.Name = "bnt_menu_esta_factura";
-            this.bnt_menu_esta_factura.Size = new System.Drawing.Size(115, 108);
-            this.bnt_menu_esta_factura.TabIndex = 1;
-            this.bnt_menu_esta_factura.Text = "Facturas";
-            this.bnt_menu_esta_factura.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.bnt_menu_esta_factura.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.bnt_menu_esta_factura.UseVisualStyleBackColor = false;
-            this.bnt_menu_esta_factura.Click += new System.EventHandler(this.bnt_menu_esta_factura_Click);
-            // 
             // bnt_menu_esta_cliente
             // 
             this.bnt_menu_esta_cliente.BackColor = System.Drawing.SystemColors.ControlLightLight;
@@ -154,7 +136,6 @@ namespace ComunicAr.Formularios.Menu
             this.Controls.Add(this.bnt_menu_esta_llamadas);
             this.Controls.Add(this.bnt_menu_esta_numero);
             this.Controls.Add(this.bnt_menu_esta_servicios);
-            this.Controls.Add(this.bnt_menu_esta_factura);
             this.Controls.Add(this.bnt_menu_esta_dispositivos);
             this.Controls.Add(this.bnt_menu_esta_cliente);
             this.Controls.Add(this.bnt_menu_esta_cancelar);
@@ -173,7 +154,6 @@ namespace ComunicAr.Formularios.Menu
         private System.Windows.Forms.Button bnt_menu_esta_dispositivos;
         private System.Windows.Forms.Button bnt_menu_esta_numero;
         private System.Windows.Forms.Button bnt_menu_esta_llamadas;
-        private System.Windows.Forms.Button bnt_menu_esta_factura;
         private System.Windows.Forms.Button bnt_menu_esta_servicios;
     }
 }
